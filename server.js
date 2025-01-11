d4bbc6b8350b8587ac2e0fc1e35753d03098c2c0
Shane Wade <shanebramble96@hotmail.com>
2025-01-10 18:40:39 -0600

    completed a POST request to add an animals to animals.json file

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 const express = require("express");
+const fs = require("fs");
+const path = require("path");
 const { animals } = require("./data/animals");
+const { type } = require("os");
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
+// parse incomming string or array data
+app.use(express.urlencoded({ extended: true }));
+// parse incomming JSON data
+app.use(express.json());
 
 function filterByQuery(query, animalsArray) {
   let personalityTraitArray = [];
@@ -54,6 +61,43 @@ function findById(id, animalsArray) {
   const result = animalsArray.filter((animals) => animals.id === id)[0];
   return result;
 }
+function createNewAnimal(body, animalsArray) {
+  // our function's main code will go here!
+  const animal = body;
+
+  // This won't actually add information to animals.json. Keep in mind that
+  // whenever we use require() to import data or functionality, it's only
+  // reading the data and creating a copy of it to use in server.js. So nothing
+  // we do with the imported data will ever affect the content of the file from
+  // which that data came. We'll have to not only use .push() to save the new
+  // data in this local server.js copy of our animal data, but we'll also have
+  // to import and use the fs library to write that data to animals.json.
+  animalsArray.push(animal);
+
+  fs.writeFileSync(
+    path.join(__dirname, "./data/animals.json"),
+    JSON.stringify({ animals: animalsArray }, null, 2)
+  );
+
+  // return finished code to post route for response.
+  return animal;
+}
+function validateAnimal(animal) {
+  if (!animal.name || typeof animal.name !== "string") {
+    return false;
+  }
+  if (!animal.species || typeof animal.species !== "string") {
+    return false;
+  }
+  if (!animal.diet || typeof animal.diet !== "string") {
+    return false;
+  }
+  if (!animal.personalityTraits || !Array.isArray(animal.personalityTraits)) {
+    return false;
+  }
+  return true;
+}
+
 app.get("/api/animals", (req, res) => {
   let results = animals;
   if (req.query) {
@@ -68,6 +112,19 @@ app.get("/api/animals/:id", (req, res) => {
     res.json(result);
   } else {
     res.render(404);
+  }
+});
+app.post("/api/animals", (req, res) => {
+  // set id based on what the next index of the array will be.
+  req.body.id = animals.length.toString();
+
+  // if any data in req,body is incorrect, send 400 error back
+  if (!validateAnimal(req.body)) {
+    res.status(400).send("The animal is not properly formatted.");
+  } else {
+    // add animal to json file and animals array in the function
+    const animal = createNewAnimal(req.body, animals);
+    res.json(req.body);
   }
 });
 
